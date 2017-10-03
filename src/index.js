3f4f8aa03d079a4ae150bf14b4e7a95e230889c2
David Chen <qydchen@gmail.com>
2017-10-03 16:19:11 -0400

    start project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import d3 from 'd3';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+// function component() {
+  // let data = ['fuck', 'you', 'hire', 'me'];
+  // d3.select(".chart")
+  //   .selectAll("div")
+  //   .data(data)
+  //     .enter()
+  //     .append("div")
+  //     .style("width", function(d) { return d + "px"; })
+  //     .text(function(d) { return d; });
+// })
+
+// document.body.appendChild(component());
